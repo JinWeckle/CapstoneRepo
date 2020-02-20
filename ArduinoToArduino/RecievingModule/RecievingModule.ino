843fbc43e816dbc4a73a64aaeb60fd0f93272e3d
Noah S <noahs9576@gmail.com>
2020-02-20 08:21:02 -0800

    bruh
    bruh

---
+++ ArduinoToArduino/RecievingModule/RecievingModule.ino
@@ -0,0 +1,55 @@
+// HM-10_Example_01_simpleLED_Peripheral
+//
+//  Pins
+//  BT VCC to Arduino 5V out. 
+//  BT GND to GND
+//  Arduino D8 (ASS RX) - BT TX no need voltage divider 
+//  Arduino D9 (ASS TX) - BT RX through a voltage divider
+//
+ 
+#include <AltSoftSerial.h>
+AltSoftSerial BTSerial; 
+ 
+char c=' ';
+byte LEDpin = 53;
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+ 
+    BTSerial.begin(9600);  
+    Serial.println("BTSerial started at 9600");
+    Serial.println(" ");
+
+    BTSerial.print("AT+RESET" );
+    Serial.print("AT+RESET");    
+    delay(1000);
+    BTSerial.print("AT+IMME0" );
+    Serial.print("AT+IMME0" );
+    delay(1000);    
+    BTSerial.print("AT+ROLE0" );
+    Serial.print("AT+ROLE0" );
+
+    
+    pinMode(LEDpin, OUTPUT); 
+    digitalWrite(LEDpin,LOW); 
+}
+ 
+void loop()
+{
+    // Read from the Bluetooth module and check if it is a command
+    if (BTSerial.available())
+    {
+        c = BTSerial.read();
+ 
+        // 49 is the ascii code for "1"
+        // 48 is the ascii code for "0"
+        if (c==49)   { digitalWrite(LEDpin,HIGH);   }
+        if (c==48)   { digitalWrite(LEDpin,LOW);    }
+        Serial.println(c);
+    }
+ 
+}
---
+++ ArduinoToArduino/SendingModule/SendingModule.ino
@@ -0,0 +1,74 @@
+// HM-10_Example_01_simpleLED_Central
+//
+//  Simple remote control using HM-10s: LED on. LED off
+//
+//
+//  Pins
+//  BT VCC to Arduino 5V out. 
+//  BT GND to GND
+//  Arduino D8 (ASS RX) - BT TX no need voltage divider 
+//  Arduino D9 (ASS TX) - BT RX through a voltage divider
+//
+ 
+#include <AltSoftSerial.h>
+AltSoftSerial BTSerial; 
+ 
+boolean pressed = false;
+
+const int buttonHigh =  50;
+const int buttonLow =  51;
+ 
+void setup() 
+{
+    pinMode(buttonHigh, INPUT);
+    pinMode(buttonLow, INPUT);
+
+    
+    Serial.begin(9600);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+ 
+    BTSerial.begin(9600);  
+    Serial.println("BTSerial started at 9600");
+    Serial.println(" ");
+ 
+    // connect to the remote Bluetooth module
+    BTSerial.print("AT+RESET" );
+    Serial.print("AT+RESET");    
+    delay(1000);
+    BTSerial.print("AT+IMME1" );
+    Serial.print("AT+IMME1" );
+    delay(1000);    
+    //BTSerial.print("AT+ROLE1" );
+    //Serial.print("AT+ROLE1" );
+    delay(1000);
+    BTSerial.print("AT+CON78DB2F140822" ); //Slave's MAC ADDR
+    Serial.print("AT+CON78DB2F140822" );
+    delay(500);
+ 
+}
+ 
+void loop()
+{ 
+  Serial.print("bH");
+  Serial.print(digitalRead(buttonLow) + "bL");
+  if(digitalRead(buttonHigh) == HIGH)
+  {
+    pressed = true;
+  }
+  if(digitalRead(buttonLow) == HIGH)
+  {
+  Serial.print("bH");
+    pressed = false;
+  }
+  if(pressed)
+  {
+    BTSerial.print("1");
+  }
+  else
+  {
+    BTSerial.print("0");
+  }
+    
+}
